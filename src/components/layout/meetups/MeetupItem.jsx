3726c45bfe0c1782363ca7454eb1c1bf717c2f23
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-18 16:26:55 +0100

    added card ui

--- src/components/layout/meetups/MeetupItem.jsx
+++ src/components/layout/meetups/MeetupItem.jsx
@@ -1,21 +1,25 @@
+import Card from "../uis/Card";
+
 const MeetupItem = ({ image, title, address, description }) => {
   return (
     <li className="mb-4">
-      <div className="w-full h-20 flex justify-center">
-        <img src={image} alt={title} />
-      </div>
-      <div className="text-center">
-        <h3 className="mb-3">{title}</h3>
-        <address className="mb-3">
-          <i>{address}</i>
-        </address>
-        <p className="mb-3">{description}</p>
-      </div>
-      <div className="flex justify-center">
-        <button className="border-1 border-pink-200 py-2 px-3 text-pink-700  rounded-xl font-semibold  ">
-          To Favorites
-        </button>
-      </div>
+      <Card>
+        <div className="w-full h-20 flex justify-center">
+          <img src={image} alt={title} />
+        </div>
+        <div className="text-center">
+          <h3 className="mb-3">{title}</h3>
+          <address className="mb-3">
+            <i>{address}</i>
+          </address>
+          <p className="mb-3">{description}</p>
+        </div>
+        <div className="flex justify-center">
+          <button className="border-1 border-pink-200 py-2 px-3 text-pink-700  rounded-xl font-semibold  ">
+            To Favorites
+          </button>
+        </div>
+      </Card>
     </li>
   );
 };
