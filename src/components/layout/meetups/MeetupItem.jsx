34f4317ec50e7fdb1e9a58ebb1d64cd31b9cc7c6
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-18 16:04:55 +0100

    added styling

--- src/components/layout/meetups/MeetupItem.jsx
+++ src/components/layout/meetups/MeetupItem.jsx
@@ -1,16 +1,20 @@
-const MeetupItem = (image, title, address, description) => {
+const MeetupItem = ({ image, title, address, description }) => {
   return (
-    <li className="m-1">
-      <div className="w-full h-20">
+    <li className="mb-4">
+      <div className="w-full h-20 flex justify-center">
         <img src={image} alt={title} />
       </div>
       <div className="text-center">
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
+        <h3 className="mb-3">{title}</h3>
+        <address className="mb-3">
+          <i>{address}</i>
+        </address>
+        <p className="mb-3">{description}</p>
       </div>
-      <div className="">
-        <button>To Favorites</button>
+      <div className="flex justify-center">
+        <button className="border-1 border-pink-200 py-2 px-3 text-pink-700  rounded-xl font-semibold  ">
+          To Favorites
+        </button>
       </div>
     </li>
   );
