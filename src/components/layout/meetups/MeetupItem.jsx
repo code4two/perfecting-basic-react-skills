f384d557d98e0e8d897cdd406e690a193012ade5
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-18 17:00:44 +0100

    added styling

--- src/components/layout/meetups/MeetupItem.jsx
+++ src/components/layout/meetups/MeetupItem.jsx
@@ -2,20 +2,20 @@ import Card from "../uis/Card";
 
 const MeetupItem = ({ image, title, address, description }) => {
   return (
-    <li className="mb-4">
+    <li className="mb-6 flex justify-center">
       <Card>
         <div className="w-full h-20 flex justify-center">
           <img src={image} alt={title} />
         </div>
-        <div className="text-center">
-          <h3 className="mb-3">{title}</h3>
+        <div className="text-center mb-8">
+          <h3 className="mb-3 font-bold text-xl">{title}</h3>
           <address className="mb-3">
             <i>{address}</i>
           </address>
           <p className="mb-3">{description}</p>
         </div>
         <div className="flex justify-center">
-          <button className="border-1 border-pink-200 py-2 px-3 text-pink-700  rounded-xl font-semibold  ">
+          <button className="border-1 border-pink-400 py-1 px-5 text-pink-700  rounded-md">
             To Favorites
           </button>
         </div>
