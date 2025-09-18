c6101f2e5294c73c2c7921e3a0d6e32857295028
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-18 17:00:15 +0100

    added styling

--- src/components/layout/uis/Card.jsx
+++ src/components/layout/uis/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="bg-white rounded-md shadow-md shadow-black shadow-opacity-10">
+    <div className="bg-white rounded-md shadow-md shadow-black shadow-opacity-10 p-5">
       {children}
     </div>
   );
