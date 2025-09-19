cddf18fdcb4e9854cb538e12b429402111b2c39e
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-19 12:56:39 +0100

    updated styling

--- src/components/layout/meetups/MeetupItem.jsx
+++ src/components/layout/meetups/MeetupItem.jsx
@@ -4,18 +4,18 @@ const MeetupItem = ({ image, title, address, description }) => {
   return (
     <li className="mb-6 flex justify-center">
       <Card>
-        <div className="w-full h-20 flex justify-center">
-          <img src={image} alt={title} />
+        <div className="flex justify-center">
+          <img className="w-full h-50" src={image} alt={title} />
         </div>
-        <div className="text-center mb-8">
+        <div className="text-center my-6">
           <h3 className="mb-3 font-bold text-xl">{title}</h3>
           <address className="mb-3">
             <i>{address}</i>
           </address>
           <p className="mb-3">{description}</p>
         </div>
-        <div className="flex justify-center">
-          <button className="border-1 border-pink-400 py-1 px-5 text-pink-700  rounded-md">
+        <div className="flex justify-center my-2">
+          <button className="border-1 border-pink-400 py-1 px-5 text-pink-700  rounded-md cursor-pointer">
             To Favorites
           </button>
         </div>
