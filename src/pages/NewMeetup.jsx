c6eacbaa3a92c8acfa147cdb570e21cd6797e2f6
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-20 16:49:00 +0100

    file updated

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -2,8 +2,8 @@ import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
   return (
-    <section className="flex flex-col justify-center items-center mt-6 font-bold text-md">
-      <h1 className="mb-5 text-3xl">Add New Meetup</h1>
+    <section className="flex flex-col justify-center items-center mt-6 font-bold text-sm">
+      <h1 className="mb-4 text-3xl">Add New Meetup</h1>
       <NewMeetupForm />
     </section>
   );
