ac19c76cb7dd46429ab6e323231901c0c168df87
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-20 23:08:18 +0100

    added post request

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const meetupDataValuesHandler = (meetupData) => {
+    fetch(
+      "https://my-first-firebase-projec-2635e-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   return (
     <section className="flex flex-col justify-center items-center mt-6 font-bold text-sm">
       <h1 className="mb-4 text-3xl">Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm meetupDataValues={meetupDataValuesHandler} />
     </section>
   );
 };
