41d0da5de9c22960e5dd914cb489fb6b6b361a25
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-20 16:16:33 +0100

    file updated

--- src/components/layout/uis/Card.jsx
+++ src/components/layout/uis/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="bg-white rounded-md shadow-md shadow-black shadow-opacity-10 p-3">
+    <div className="bg-white rounded-md shadow-md shadow-black shadow-opacity-40 p-3">
       {children}
     </div>
   );
