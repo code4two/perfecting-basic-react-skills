e4739b91e9a055e155ca06b0ed645b330f2e754d
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-20 16:17:20 +0100

    rendered form

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
 const NewMeetupPage = () => {
-  return <div>New Meetups here</div>;
+  return (
+    <section className="flex flex-col justify-center items-center mt-6 font-bold text-md">
+      <h1 className="mb-5 text-3xl">Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
