4e435152cc99b90e37fbb853db1c535ff56a3e8c
Peter Adepoju <peteradepoju22@gmail.com>
2025-09-21 16:02:36 +0100

    changed meetuplist from dummy_Data to loaded_Data fetched from API endpoint

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,33 +1,39 @@
+import { useState, useEffect } from "react";
+
 import MeetupList from "../components/layout/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meeetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a second, amazing meetup which you definitely should not miss. It will be a lot fun!",
-  },
-];
-
 const AllMeetupPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://my-first-firebase-projec-2635e-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setLoadedMeetups(data);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="flex items-center justify-center my-6">
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section className="mt-4">
       <h1 className="font-bold text-2xl flex justify-center mb-3">
         All Meetups
       </h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 };
